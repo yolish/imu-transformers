2ad92464dc62b7235f55c88307398827ab94dc7d
yolish <yolisha@gmail.com>
2020-12-13 14:26:09 +0200

    Update prepare_imu_location_recognition_labels.py
    Toy script for data generation

--- scripts/prepare_imu_location_recognition_labels.py
+++ scripts/prepare_imu_location_recognition_labels.py
@@ -1,4 +1,15 @@
+import numpy as np
+import pandas as pd
 """
 Script for preparing labels for location recognition from IMU
 """
-#TBA add logic
+# Completely senseless dataset - just for initial debugging
+window_size = 100
+n_samples = 10
+vals = np.random.random((n_samples*window_size,6))
+d = {}
+for i in range(6):
+    d["x{}".format(i)] = vals[:, i]
+d["cls"] = np.zeros(n_samples*window_size)
+df = pd.DataFrame(d)
+df.to_csv("nonsense_imu_lr_dataset.csv", index=False)
