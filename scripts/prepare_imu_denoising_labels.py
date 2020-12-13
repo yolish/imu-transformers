708ba04e184c205b56a8edb26b5395dfbf6e7039
yolish <yolisha@gmail.com>
2020-12-13 14:26:05 +0200

    Update prepare_imu_denoising_labels.py
    Toy script for data generation

--- scripts/prepare_imu_denoising_labels.py
+++ scripts/prepare_imu_denoising_labels.py
@@ -1,4 +1,15 @@
 """
 Script for preparing labels for IMU denoising task
 """
-#TBA add logic
+import numpy as np
+import pandas as pd
+
+# Completely senseless dataset - just for initial debugging
+window_size = 100
+n_samples = 10
+vals = np.random.random((n_samples*window_size,12))
+d = {}
+for i in range(12):
+    d["x{}".format(i)] = vals[:, i]
+df = pd.DataFrame(d)
+df.to_csv("nonsense_imu_denoising_dataset.csv", index=False)
