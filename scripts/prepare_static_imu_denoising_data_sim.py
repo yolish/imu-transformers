3a48d4cef4a70b38448e1ebd3f5417cb205ea340
alexs <alex.saraev@huawei.com>
2021-01-02 11:10:52 +0200

    Created noised data generation of random static accelerometer

--- scripts/prepare_static_imu_denoising_data_sim.py
+++ scripts/prepare_static_imu_denoising_data_sim.py
@@ -18,27 +18,27 @@ w_noise = b_w + w_w
 gyro_vec_noised = gyro_vec_gt + w_noise   #noised
 
 #plotting
-plt.subplot(2, 1, 1)
-plt.plot(gyro_vec_gt[:, 0], label='phi')
-plt.plot(gyro_vec_gt[:, 1], label='theta')
-plt.plot(gyro_vec_gt[:, 2], label='psy')
-plt.title('Omega')
-plt.xlabel('Num of samples')
-plt.ylabel('[rad/sec]')
-plt.legend()
-plt.grid()
-
-plt.subplot(2, 1, 2)
-plt.plot(gyro_vec_noised[:, 0], label='phi')
-plt.plot(gyro_vec_noised[:, 1], label='theta')
-plt.plot(gyro_vec_noised[:, 2], label='psy')
-plt.title('Omega Noised')
-plt.xlabel('Num of samples')
-plt.ylabel('[rad/sec]')
-plt.legend()
-plt.grid()
-if plot_on:
-    plt.show()
+# plt.subplot(2, 1, 1)
+# plt.plot(gyro_vec_gt[:, 0], label='phi')
+# plt.plot(gyro_vec_gt[:, 1], label='theta')
+# plt.plot(gyro_vec_gt[:, 2], label='psy')
+# plt.title('Omega')
+# plt.xlabel('Num of samples')
+# plt.ylabel('[rad/sec]')
+# plt.legend()
+# plt.grid()
+#
+# plt.subplot(2, 1, 2)
+# plt.plot(gyro_vec_noised[:, 0], label='phi')
+# plt.plot(gyro_vec_noised[:, 1], label='theta')
+# plt.plot(gyro_vec_noised[:, 2], label='psy')
+# plt.title('Omega Noised')
+# plt.xlabel('Num of samples')
+# plt.ylabel('[rad/sec]')
+# plt.legend()
+# plt.grid()
+# if plot_on:
+#     plt.show()
 
 
 # Saving the data
@@ -57,10 +57,69 @@ theta = np.random.rand()*np.pi/2
 print(theta)
 g = 9.806
 angles_vec = np.array([-np.sin(theta), np.sin(phi)*np.cos(theta), np.cos(phi)*np.cos(theta)])
-accel_vec = angles_vec*g
 print(angles_vec)
+accel_vec = angles_vec*g
+print(np.linalg.norm(accel_vec))
 accel_seq = np.vstack([accel_vec]*num_samples)
 print(accel_seq)
 
 
-a = 5
+# plt.plot(accel_seq[:, 0], label='accel_x')
+# plt.plot(accel_seq[:, 1], label='accel_y')
+# plt.plot(accel_seq[:, 2], label='accel_z')
+# plt.title('Omega')
+# plt.xlabel('Num of samples')
+# plt.ylabel('[m/sec^2]')
+# plt.legend()
+# plt.grid()
+# plt.show()
+
+def generate_accel_rand(num_samples):
+    phi = np.random.rand() * np.pi/2
+    theta = np.random.rand() * np.pi/2
+    g = 9.806
+    angles_vec = np.array([-np.sin(theta), np.sin(phi) * np.cos(theta), np.cos(phi) * np.cos(theta)])
+    accel_vec = angles_vec * g
+    accel_seq = np.vstack([accel_vec] * num_samples)
+    return accel_seq
+
+
+num_of_seq = 3
+num_samples = 1000
+accel_seq_all = generate_accel_rand(num_samples)
+
+for i in range(num_of_seq):
+    current_seq = generate_accel_rand(num_samples)
+    accel_seq_all = np.vstack([accel_seq_all, current_seq])
+
+
+b_f = (20*(1e-3))/9.81
+# generating white noise
+mu_wf = 0
+sigma_wf = 500*0.2*(1e-2)/9.81
+w_f = np.random.normal(mu_wf, sigma_wf, (len(accel_seq_all), 3))
+f_noise = b_f + w_f
+accel_seq_all_noised = accel_seq_all + f_noise  #noised
+
+
+plt.subplot(2, 1, 1)
+plt.plot(accel_seq_all[:, 0], label='accel_x')
+plt.plot(accel_seq_all[:, 1], label='accel_y')
+plt.plot(accel_seq_all[:, 2], label='accel_z')
+plt.title('Acceleration')
+plt.xlabel('Num of samples')
+plt.ylabel('[m/sec^2]')
+plt.legend()
+plt.grid()
+
+
+plt.subplot(2, 1, 2)
+plt.plot(accel_seq_all_noised[:, 0], label='accel_x')
+plt.plot(accel_seq_all_noised[:, 1], label='accel_y')
+plt.plot(accel_seq_all_noised[:, 2], label='accel_z')
+plt.title('Acceleration Noised')
+plt.xlabel('Num of samples')
+plt.ylabel('[m/sec^2]')
+plt.legend()
+plt.grid()
+plt.show()
